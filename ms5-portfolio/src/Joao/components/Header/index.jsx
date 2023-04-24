8c0cfe2bb2ba906ebec278c5bc091e8ae07f919c
Joao Rocha <rjoao0112@gmail.com>
2023-04-24 14:03:31 +0100

    Header Implementation

---
+++ ms5-portfolio/src/Joao/components/Header/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import {
+  Wrapper,
+  LogoLink,
+  NavList,
+  NavItemLink,
+  HamburgerMenu,
+  UserName,
+  Sidebar,
+  SidebarLink, 
+  SidebarUserName,
+  CloseButton
+} from "./styles";
+import minderaLogo from "../../images/minderaLogo.png";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Wrapper>
+      <LogoLink to="/">
+        <img src={minderaLogo} alt="Mindera Logo" />
+      </LogoLink>
+      <NavList className={isOpen ? "show" : ""}>
+        <li>
+          <NavItemLink href="#projects">Projects</NavItemLink>
+        </li>
+        <li>
+          <NavItemLink href="#what-i-use">What I use</NavItemLink>
+        </li>
+        <li>
+          <NavItemLink href="#about-me">About me</NavItemLink>
+        </li>
+        <li>
+          <NavItemLink href="#contact">Contact</NavItemLink>
+        </li>
+      </NavList>
+      <UserName>João Rocha</UserName>
+      <HamburgerMenu isOpen={isOpen} onClick={toggleMenu}>
+        <span />
+        <span />
+        <span />
+      </HamburgerMenu>
+      <Sidebar isOpen={isOpen}>
+        <CloseButton onClick={toggleMenu}>
+          <FaTimes />
+        </CloseButton>
+        <SidebarLink href="#projects">Projects</SidebarLink>
+        <SidebarLink href="#what-i-use">What I use</SidebarLink>
+        <SidebarLink href="#about-me">About me</SidebarLink>
+        <SidebarLink href="#contact">Contact</SidebarLink>
+        <SidebarUserName>João Rocha</SidebarUserName>
+      </Sidebar>
+    </Wrapper>
+  );
+}
+
+export default Header;
---
+++ ms5-portfolio/src/Joao/components/Header/styles.jsx
@@ -0,0 +1,173 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Wrapper = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background-color: white;
+
+  @media (max-width: 768px) {
+    padding: 0 10px;
+  }
+`;
+
+export const LogoLink = styled(Link)`
+  img {
+    width: 50px;
+  }
+
+  @media (max-width: 768px) {
+    img {
+      width: 40px;
+    }
+  }
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  @media (max-width: 768px) {
+    display: none;
+    position: absolute;
+    top: 80px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: white;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  li {
+    margin-right: 20px;
+
+    @media (max-width: 768px) {
+      margin: 10px 0;
+    }
+  }
+`;
+
+export const NavItemLink = styled.a`
+  color: black;
+  text-decoration: none;
+  transition: color 0.2s ease-in-out;
+  font-weight: bold;
+  font-size: 24px;
+
+  &:hover {
+    color: #c1a615;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+export const HamburgerMenu = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    cursor: pointer;
+  }
+
+  span {
+    display: block;
+    width: 25px;
+    height: 3px;
+    margin: 5px 0;
+    background-color: black;
+  }
+
+  &:hover {
+    & span {
+      transition: background-color 0.2s ease-in-out;
+      background-color: #c1a615;
+    }
+  }
+`;
+
+export const UserName = styled.p`
+  margin: 0;
+  font-weight: bold;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const Sidebar = styled.div`
+  position: fixed;
+  top: 0;
+  right: ${({ isOpen }) => (isOpen ? "-60%" : "-108%")};
+  height: 100vh;
+  width: 200px;
+  background-color: white;
+  padding: 60px 20px;
+  transition: padding 0.3s ease-in-out, right 0.3s ease-in-out;
+  z-index: 10;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const SidebarLink = styled.a`
+  display: block;
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 20px;
+  transition: color 0.2s ease-in-out;
+
+  &:hover {
+    color: #c1a615;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+export const SidebarUserName = styled.span`
+  position: absolute;
+  bottom: 135px;
+  right: 60%;
+  font-weight: bold;
+`;
+
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 59%;
+  border: none;
+  background-color: transparent;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    color: #c1a615;
+  }
+`;
