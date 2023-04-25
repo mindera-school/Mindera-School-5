82b27c86ef82800578a59ea3231883183156c10a
Joao Rocha <rjoao0112@gmail.com>
2023-04-25 19:51:07 +0100

    Provide repos on Main

--- ms5-portfolio/src/Joao/MainJoao.js
+++ ms5-portfolio/src/Joao/MainJoao.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Header from "./components/Header";
-import {GlobalStyle, Body} from "./mainJoaoStyles";
+import { GlobalStyle, Body } from "./mainJoaoStyles";
+import { Helmet } from "react-helmet";
+import minderaLogo from "./images/minderaLogo.png";
+import { ReposProvider } from "./services/RepoService";
 
 export default function Main() {
-    return (
-    <Body>
+  return (
+    <ReposProvider>
+      <Body>
         <GlobalStyle />
+        <Helmet>
+          <title>Rocha Portfolio</title>
+          <link
+            rel="shortcut icon"
+            href={minderaLogo}
+            sizes="64x64"
+            type="image/png"
+          />
+        </Helmet>
         <Header></Header>
-    </Body>)
+      </Body>
+    </ReposProvider>
+  );
 }
